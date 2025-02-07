498be843bcb04df232b597d51b71c67661b8e403
Arunkumar <arunkumar.e@laderatechnology.com>
2025-02-07 17:40:49 +0530

    Added tab for customer, address, product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,22 @@ import { ProductListComponent } from './home-page/admin/product/product-list/pro
 import { AddProductComponent } from './home-page/admin/product/add-product/add-product.component';
 import { CreateAddressComponent } from './home-page/admin/address/create-address/create-address.component';
 import { AddressListComponent } from './home-page/admin/address/address-list/address-list.component';
+import { InventoryComponent } from './home-page/inventory/inventory.component';
+import { OrdersComponent } from './home-page/orders/orders.component';
+
 const routes: Routes = [
-  { path: 'customer/edit/:customerId', component: AddCustomerComponent, canActivate: [AuthGuard] }, // For update
-  { path: 'customer/add', component: AddCustomerComponent, canActivate: [AuthGuard] },
-  { path: 'customer', component: CustomerListComponent, canActivate: [AuthGuard] },
-  { path: 'products', component: ProductListComponent, canActivate: [AuthGuard] },
-  { path: 'product/add', component: AddProductComponent, canActivate: [AuthGuard] },
-  { path: 'product/edit', component: AddProductComponent, canActivate: [AuthGuard] },
-    {path: 'address/add', component:CreateAddressComponent, canActivate: [AuthGuard]},
-  {path: 'address/edit/:id', component:CreateAddressComponent, canActivate: [AuthGuard]},
-  {path:'address/list',component:AddressListComponent, canActivate: [AuthGuard]}
+    { path: 'customer/edit/:customerId', component: AddCustomerComponent, canActivate: [AuthGuard] },
+    { path: 'customer/add', component: AddCustomerComponent, canActivate: [AuthGuard] },
+    { path: 'customer', component: CustomerListComponent, canActivate: [AuthGuard] },
+    { path: 'products', component: ProductListComponent, canActivate: [AuthGuard] },
+    { path: 'product/add', component: AddProductComponent, canActivate: [AuthGuard] },
+    { path: 'product/edit', component: AddProductComponent, canActivate: [AuthGuard] },
+    { path: 'address/add', component: CreateAddressComponent, canActivate: [AuthGuard] },
+    { path: 'address/edit/:id', component: CreateAddressComponent, canActivate: [AuthGuard] },
+    { path: 'address/list', component: AddressListComponent, canActivate: [AuthGuard] },
+    { path: 'order', component: OrdersComponent, canActivate: [AuthGuard] },
+    { path: 'inventory', component: InventoryComponent, canActivate: [AuthGuard] },
+    { path: '', redirectTo: 'inventory', pathMatch: 'full' },
 ];
 
 @NgModule({
