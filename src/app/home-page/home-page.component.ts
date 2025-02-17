13d44bfbd8f00680bd9bc720b39187838be12a1e
Arunkumar <arunkumar.e@laderatechnology.com>
2025-02-17 18:44:00 +0530

    tab and dropdown selection issue fixed

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -11,42 +11,53 @@ export class HomePageComponent implements OnInit {
     { label: 'Inventory', link: '/inventory' },
     { label: 'Order', link: '/order' }
   ];
-  linkDropdownList =[
-    {name: 'Customer', link: '/customer/list'},
-    {name: 'Product', link: '/product'},
-    {name: 'Address', link: '/address/list'},
-    {name:'Location',link:'/location/list'},
-    {name: 'Client', link: '/client'},
-    {name: 'Roles', link: '/listrole'},
-  ]
-  activeTabIndex = 0;
+  linkDropdownList = [
+    { name: 'Customer', link: '/customer/list' },
+    { name: 'Product', link: '/product' },
+    { name: 'Address', link: '/address/list' },
+    { name: 'Location', link: '/location/list' },
+    { name: 'Client', link: '/client' },
+    { name: 'Roles', link: '/listrole' },
+  ];
+
+  activeTabIndex: number = -1;
+  activeDropdownItem: string | null = null;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        const index = this.tabs.findIndex(tab => event.urlAfterRedirects.includes(tab.link));
-        if (index !== -1) {
-          this.activeTabIndex = index;
-        }
+        this.updateActiveTab(event.urlAfterRedirects);
       }
     });
+    this.updateActiveTab(this.router.url);
   }
 
+  private updateActiveTab(currentUrl: string) {
+    const tabIndex = this.tabs.findIndex(tab => currentUrl.startsWith(tab.link));
+    const dropdownItem = this.linkDropdownList.find(item => currentUrl.startsWith(item.link));
+    if (tabIndex !== -1) {
+      this.activeTabIndex = tabIndex;
+      this.activeDropdownItem = null;
+    } else if (dropdownItem) {
+      if (this.activeTabIndex !== -1) {
+        return;
+      }
+      this.activeDropdownItem = dropdownItem.link;
+      this.activeTabIndex = -1;
+    }
+  }
+  
   onTabChange(index: number) {
     this.activeTabIndex = index;
+    this.activeDropdownItem = null;
     this.router.navigate([this.tabs[index].link]);
   }
 
-  onDropdownSelect(link: string, label: string) {
-    const existingTabIndex = this.tabs.findIndex(tab => tab.link === link);
-    if (existingTabIndex === -1) {
-      this.tabs.push({ label, link });
-      this.activeTabIndex = this.tabs.length - 1;
-    } else {
-      this.activeTabIndex = existingTabIndex;
-    }
+  onDropdownSelect(link: string) {
+    this.activeDropdownItem = link;
+    this.activeTabIndex = -1;
     this.router.navigate([link]);
   }
 }
